c440356fed8f2a3e0d5773af9feaa85501f2a427
Kenta <Kenta@192.168.11.17>
2013-06-26 19:55:44 +0900

    t

---
+++ AndroidSample/gen/androidsample/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package androidsample.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ AndroidSample/src/androidsample/test/SampleActivity.java
@@ -0,0 +1,43 @@
+package androidsample.test;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class SampleActivity extends Activity implements OnClickListener{
+
+	private int count1 = 0, count2 = 0;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sample);
+		Button[] bt = new Button[2];
+		bt[0] = (Button)findViewById(R.id.button1); //findViewByidで、viewのボタンを呼び出している。
+		bt[1] = (Button)findViewById(R.id.button2);
+		for(int i=0; i<bt.length; i++){
+			bt[i].setOnClickListener(this); //SampleActivityクラスがOnClickListenerインターフェースを実装しているため、
+										//thisと入力する。
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.sample, menu);
+		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO 自動生成されたメソッド・スタブ
+		Button bt = (Button) v;
+		if(v.getId() == R.id.button1) bt.setText(++count1+"回目");
+		else bt.setText(++count2+"回目");
+	}
+	
+	//コミットテストｗ
+	//テスてす
+
+}
